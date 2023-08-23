285afbf7246c4df99e129be1c309d00c1ed2dfac
kigen chesire <ekchesire10@gmail.com>
2023-08-23 22:25:18 +0300

    adding dummy data

--- past papers/routes/channels.php
+++ past papers/routes/channels.php
@@ -2,12 +2,6 @@
 
 use Illuminate\Support\Facades\Broadcast;
 
-/*
-|--------------------------------------------------------------------------
-| Broadcast Channels
-|--------------------------------------------------------------------------
-
-
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
